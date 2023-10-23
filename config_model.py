36e3d130f5df3c3136a3b3e7a5b226797d22344b
Duy <meomeoaww@gmail.com>
2023-10-23 17:38:08 +0700

    add 3 file Config, Train, Model

---
+++ config_model.py
@@ -0,0 +1,115 @@
+#config_model
+import cv2
+import numpy as np
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+TIME_STEPS = 240
+test_size = 0.2
+resize_max_width=0
+
+training_img = []
+training_txt = []
+train_input_length = []
+train_label_length = []
+orig_txt = []
+
+valid_img = []
+valid_txt = []
+valid_input_length = []
+valid_label_length = []
+valid_orig_txt = []
+
+train_image_paths, val_image_paths = train_test_split(all_image_paths , test_size=test_size, random_state=42)
+
+for train_img_path in train_image_paths:
+    # print(f_name)
+    # read input image and convert into gray scale image
+    img = cv2.cvtColor(cv2.imread(train_img_path), cv2.COLOR_BGR2GRAY)
+    
+    height, width = img.shape
+
+    # in this dataset, we don't need to do any resize at all here.
+    img = cv2.resize(img,(int(118/height*width),118))
+    
+    height, width = img.shape
+    
+    if img.shape[1] > resize_max_width:
+        resize_max_width = img.shape[1]
+
+    img = np.pad(img, ((0,0),(0, 2167-width)), 'median')
+    
+    # YOUR PART: Blur it
+    img = cv2.GaussianBlur(img, (5,5), 0)
+
+    # YOUR PART: Threshold the image using adapative threshold
+    img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 4)
+    
+    # add channel dimension
+    img = np.expand_dims(img , axis = 2)
+    
+    # Normalize each image
+    img = img/255.
+
+    label = dict_filepath_label[train_img_path]
+
+    # split data into validation and training dataset as 10% and 90% respectively
+    orig_txt.append(label)   
+    train_label_length.append(len(label))
+
+    # our time steps for valid input
+    train_input_length.append(TIME_STEPS)
+    training_img.append(img)
+
+    # convert words to digits based on charlist
+    training_txt.append(encode_to_labels(label)) 
+
+valid_img = []
+valid_txt = []
+valid_input_length = []
+valid_label_length = []
+valid_orig_txt = []
+
+for val_img_path in val_image_paths:
+    # print(f_name)
+    # read input image and convert into gray scale image
+    img = cv2.cvtColor(cv2.imread(val_img_path), cv2.COLOR_BGR2GRAY)
+    
+    # in this dataset, we don't need to do any resize at all here.
+    img = cv2.resize(img,(int(118/height*width),118))
+    
+    if img.shape[1] > resize_max_width:
+        resize_max_width = img.shape[1]
+        
+    img = np.pad(img, ((0,0),(0, 2167-width)), 'median')
+    
+    # YOUR PART: Blur it
+    img = cv2.GaussianBlur(img, (5,5), 0)
+
+    # YOUR PART: Threshold the image using adapative threshold
+    img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 4)
+    
+    # add channel dimension
+    img = np.expand_dims(img , axis = 2)
+    
+    # Normalize each image
+    img = img/255.
+
+    label = dict_filepath_label[val_img_path]
+
+    valid_orig_txt.append(label)   
+    valid_label_length.append(len(label))
+
+    # our time steps for valid input
+    valid_input_length.append(TIME_STEPS)
+    valid_img.append(img)
+
+    # convert words to digits based on charlist
+    valid_txt.append(encode_to_labels(label))
+ 
+
+max_label_len = TIME_STEPS
+
+# pad each output label to maximum text length, remember we did that so that we keep training with rnn consistent?
+train_padded_txt = pad_sequences(training_txt, maxlen=max_label_len, padding='post', value = 0)
+valid_padded_txt = pad_sequences(valid_txt, maxlen=max_label_len, padding='post', value = 0)
