54d4112dd8101d36b6a41c2011dd518ee3d20587
katurtr <canhhxse170064@fpt.edu.vn>
2023-11-15 23:55:40 +0700

    update model

--- OCR_Website/app/Models/__init__.py
+++ OCR_Website/app/Models/__init__.py
@@ -1,18 +1,109 @@
 from Model import *
 import sys
 import cv2
-from torch import cuda
+import torch
 import time
 
 import gradio as gr
-from transformers import TrOCRProcessor, VisionEncoderDecoderModel
+from transformers import TrOCRProcessor, VisionEncoderDecoderModel, AutoConfig
+from transformers.modeling_outputs import Seq2SeqLMOutput
 import requests
 from PIL import Image
 import pytesseract
 
+class BetterHFTrOCR(VisionEncoderDecoderModel):
+    """creates a TrOCR model"""
 
-# device = 'cuda' if cuda.is_available() else 'cpu'
+    def __init__(self, model_path):
+        model_ = VisionEncoderDecoderModel.from_pretrained(model_path)
 
+        super().__init__(model_.config)
+
+        self.encoder = model_.encoder
+        self.decoder = model_.decoder
+
+    def forward(
+        self,
+        pixel_values=None,
+        decoder_input_ids=None,
+        decoder_attention_mask=None,
+        encoder_outputs=None,
+        past_key_values=None,
+        decoder_inputs_embeds=None,
+        labels=None,
+        use_cache=None,
+        output_attentions=None,
+        output_hidden_states=None,
+        return_dict=None,
+        **kwargs,
+    ):
+
+        encoder_hidden_states = encoder_outputs['last_hidden_state'] \
+                                 if type(encoder_outputs)==dict \
+                                 else encoder_outputs[0]
+
+        encoder_attention_mask = None
+        
+        eos_mask = decoder_input_ids[:, -1] <= self.config.eos_token_id
+
+        # Decode        
+        if any(eos_mask) and (decoder_input_ids.shape[1]>1):
+            reduced_logits = self.decoder(
+                ### compute reduction ###
+                input_ids=decoder_input_ids[torch.logical_not(eos_mask), :],
+                attention_mask=decoder_attention_mask[torch.logical_not(eos_mask), :],
+                encoder_hidden_states=encoder_hidden_states[torch.logical_not(eos_mask), :, :],
+                #########################
+                encoder_attention_mask=encoder_attention_mask,
+                inputs_embeds=decoder_inputs_embeds,
+                output_attentions=output_attentions,
+                output_hidden_states=output_hidden_states,
+                use_cache=use_cache,
+                past_key_values=past_key_values,
+            ).logits
+            
+            logits = torch.full((decoder_input_ids.shape[0], decoder_input_ids.shape[1], self.config.decoder.vocab_size), fill_value=self.config.pad_token_id, dtype=reduced_logits.dtype, device=reduced_logits.device)
+            logits[torch.logical_not(eos_mask), :, :] = reduced_logits
+            logits[eos_mask, :, :] = self.ids_to_logits(decoder_input_ids[eos_mask, 1:], reduced_logits)
+        else:
+            logits = self.decoder(
+                input_ids=decoder_input_ids,
+                attention_mask=decoder_attention_mask,
+                encoder_hidden_states=encoder_hidden_states,
+                encoder_attention_mask=encoder_attention_mask,
+                inputs_embeds=decoder_inputs_embeds,
+                output_attentions=output_attentions,
+                output_hidden_states=output_hidden_states,
+                use_cache=use_cache,
+                past_key_values=past_key_values,
+            ).logits
+
+        return Seq2SeqLMOutput(
+          logits=logits,
+        )
+
+    def ids_to_logits(self, ids, reduced_logits):
+        logits = torch.zeros((ids.shape[0], ids.shape[1]+1, self.config.decoder.vocab_size), dtype=reduced_logits.dtype, device=reduced_logits.device)
+        logits[:, -1, 2] = 1 # max_pad_token
+        for i in range(ids.shape[1]):
+            logits[:, i, ids[:, i]] = 1
+        
+        return logits
+
+
+
+def configure_generation(model, processor, beams=1):
+    # model.config.decoder_start_token_id = preprocessor.tokenizer.cls_token_id # only if you're gonna train the model
+    model.config.pad_token_id = model.config.decoder.pad_token_id = processor.tokenizer.pad_token_id
+    model.config.eos_token_id = model.config.decoder.eos_token_id = processor.tokenizer.sep_token_id
+    # make sure vocab size is set correctly
+    model.config.vocab_size = model.config.decoder.vocab_size
+    # set beam search parameters
+    model.config.decoder.early_stopping = True
+    model.config.decoder.no_repeat_ngram_size = 3
+    model.config.decoder.length_penalty = 2.0
+    model.config.decoder.num_beams = beams
+    
 def process_image(image): 
     # prepare image
     pixel_values = processor(image, return_tensors="pt").pixel_values
@@ -28,7 +119,9 @@ def process_image(image):
 def prediction(image_path):
     global processor, model, custom_config
     processor = TrOCRProcessor.from_pretrained("microsoft/trocr-base-handwritten")
-    model = VisionEncoderDecoderModel.from_pretrained("C:/xampp/htdocs/OCR_Website/app/Models/text_detection/checkpoint-1000")
+    model = BetterHFTrOCR(model_path = "C:/xampp/htdocs/OCR_Website/app/Models/text_detection/checkpoint-3000").to("cpu")
+    
+    configure_generation(model, processor)
     
     # model.to(device)
     
@@ -71,16 +164,16 @@ def prediction(image_path):
 
     # Display each cropped region
     for region in cropped_regions:
-        image = Image.fromarray(region)
-        image = image.convert("RGB")
-        # print(process_image(image))
+        image_fromarray = Image.fromarray(region)
+        image = image_fromarray.convert("RGB")
+        print(process_image(image))
         Lit.append(process_image(image))
 
     return Lit
 
 if __name__ == "__main__":
-    image_path = sys.argv[1]
-    # image_path = 'C:/xampp/htdocs/OCR_Website/app/Models/data/for_testing_code/image.png'
+    # image_path = sys.argv[1]
+    image_path = 'C:/xampp/htdocs/OCR_Website/app/Models/text_detection/Tesseract-OCR/image/dontu2.jpg'
     # prediction(image_path)
     output_data = prediction(image_path)
     output = ' \n'.join(output_data)
